3c44886c0f2851de904e258f65f46c70a3ead49f
_LegendaX_ <you@abc.net>
2024-12-20 20:38:43 +0300

    final

--- views.py
+++ views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
-import time
+from datetime import datetime
 
 def index_page(request):
     context = {
         "name": "Ярослав",
-        "pages_count": 2
+        "pages_count": 3
     }
     return render(request, "index.html", context)
 
 def time_page(request):
+    now=datetime.now()
     context = {
-        "data": time,
-        "time":  time.strftime('%H:%M:%S %p')
+        "data": now.strftime("%d-%m-%Y"),
+        "time":  now.strftime('%H:%M:%S %p')
     }
     return render(request, "time.html", context)
+
+def calc_page (request):
+    a = int(request. GET.get('a', 0))
+    b = int(request. GET. get('b', 0))
+    context = {
+    "titLe":'Курс "Промышленное программирование"',
+    "a": a,
+    "b":b,
+    "sum": a + b
+    }
+    return render(request, "calc.html", context)
